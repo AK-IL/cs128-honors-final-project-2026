9df647d293519d72478d3cbf7c073410a4fe5ffb
AK-IL <akhanna7@illinois.edu>
2026-04-11 01:43:03 +0000

    Add basic documentation

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,12 +1,18 @@
+// Note: This implementation is heavily based on simple plaintext AES encryption guides
+
 use aes_gcm::{aead::{Aead, KeyInit}, Aes256Gcm, Nonce};
 use rand::{rngs::OsRng, RngCore};
 
 pub fn encrypt_txt(key: &[u8; 32], txt: &str) -> Vec<u8> {
     let cipher = Aes256Gcm::new_from_slice(key).expect("Key length invalid");
+    
+    // Makes sure every session is unique (https://en.wikipedia.org/wiki/Cryptographic_nonce)
     let mut nb = [0u8; 12];
     OsRng.fill_bytes(&mut nb);
     let nonce = Nonce::from_slice(&nb);
     let mut encrypted = cipher.encrypt(nonce, txt.as_bytes()).expect("Failed to encrypt");
+    
+    // Bundle text and nonce
     let mut packet = nb.to_vec();
     packet.append(&mut encrypted);
     packet
@@ -17,8 +23,12 @@ pub fn decrypt_txt(key: &[u8; 32], mut msg: Vec<u8>) -> Option<String> {
         return None;
     }
     let cipher = Aes256Gcm::new_from_slice(key).expect("Invalid key");
+    
+    // Chops off the 12 byte nonce
     let nb = msg.drain(..12).collect::<Vec<u8>>();
     let nonce = Nonce::from_slice(&nb);
+    
+    // Detects if message was tampered with (any byte(s) changed, nonce replaced, truncation, wrong key, etc.) and returns None if so
     let tb = cipher.decrypt(nonce, msg.as_ref()).ok()?;
     String::from_utf8(tb).ok()
 }
--- src/key_exchange.rs
+++ src/key_exchange.rs
@@ -1,3 +1,5 @@
+// Note: This implementation is heavily based on simple Diffie-Hellman guides
+
 use rand::rngs::OsRng;
 use x25519_dalek::{StaticSecret, PublicKey};
 use hkdf::Hkdf;
@@ -15,25 +17,31 @@ impl Drop for KeyPair {
     }
 }
 
+// Generates the user's public and private keys
 pub fn gen_keys() -> KeyPair {
     let secret = StaticSecret::random_from_rng(OsRng);
     let public = PublicKey::from(&secret);
     KeyPair { secret, public }
 }
 
+// Converts the private key to bytes for easier network operations
 pub fn public_bytes(public: &PublicKey) -> [u8; 32] {
     public.to_bytes()
 }
 
+// Converts bytes back into a public key
 pub fn public_from_bytes(bytes: [u8; 32]) -> PublicKey {
     PublicKey::from(bytes)
 }
 
+// Diffie-Hellman exchange
 pub fn get_shared_key(private: &StaticSecret, ext_public: &PublicKey) -> [u8; 32] {
     let shared = private.diffie_hellman(ext_public);
     let key_deriver = Hkdf::<Sha256>::new(None, shared.as_bytes());
     let mut out = [0u8; 32];
+    
+    // HKDF key derivation to turn diffie-hellman output into a usable AES key
     key_deriver.expand(&[], &mut out).expect("Key derivation failed");
-    drop(shared);
+    drop(shared); // <-- Not sure if this is necessary or if x25519-dalek already does it
     out
 }
