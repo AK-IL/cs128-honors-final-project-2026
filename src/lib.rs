29c4ab4ed09ffef130c55c9db9394ec265be0d8f
AK-IL <akhanna7@illinois.edu>
2026-04-11 02:54:20 +0000

    Much better API

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,23 @@ pub mod crypto;
 use serde::{Serialize, Deserialize};
 
 // Protocol so client & server are in agreement
-#[derive(Serialize, Deserialize, Debug)]
-pub enum MsgProtocol {
-    DiffieHellman([u8; 32]), // Diffie-Hellman public key exchange bytes
-    Etxt(Vec<u8>), // Encrypted txt (w/ nonce)s
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Navigation {
+    Everyone, // Send to everyone
+    Room(String), // Send to group
+    User(String), // Send to person
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Data {
+    KeyExchange([u8; 32]), // Diffie-Hellman key exchnage
+    Text(Vec<u8>), // Encrypted plaintext message (including the nonce)
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Packet {
+    pub navi: Navigation, // Destination
+    pub timestamp: u64, // Timestamp
+    pub source: String, // Sender
+    pub data: Data, // Message content
 }
