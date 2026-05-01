b28bcebe56e2bc3a84e128f38a62fb50200f0b17
AK-IL <akhanna7@illinois.edu>
2026-05-01 16:20:13 +0000

    Allow routing messages globally, to rooms, and directly. Uses Atomic Reference Counting via Arc<T> for easier memory management.

--- src/relay.rs
+++ src/relay.rs
@@ -2,21 +2,27 @@
 // Ignore the comments if they are too much, they are there to explain tokio specific code
 
 use tokio::net::TcpListener;
-use tokio::sync::broadcast;
+use tokio::sync::{broadcast, mpsc};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
-use crate::Packet;
+use std::collections::{HashMap, HashSet};
+use std::sync::{Arc, RwLock};
+use crate::{Packet, Navigation};
 
 pub struct Relay {
     address: String,
-    transmitter: broadcast::Sender<String>,
+    clients: Arc<RwLock<HashMap<String, mpsc::UnboundedSender<String>>>>,
+    rooms: Arc<RwLock<HashMap<String, HashSet<String>>>>,
+    global_tx: broadcast::Sender<String>,
 }
 
 impl Relay {
     pub fn new(addr: &str, buffer: usize) -> Self {
-        let (transmitter, _) = broadcast::channel(buffer);
+        let (global_tx, _) = broadcast::channel(buffer);
         Self {
             address: addr.to_string(),
-            transmitter,
+            clients: Arc::new(RwLock::new(HashMap::new())),
+            rooms: Arc::new(RwLock::new(HashMap::new())),
+            global_tx,
         }
     }
     
@@ -30,33 +36,88 @@ impl Relay {
 
         loop {
             let (mut socket, _addr) = tcp_listener.accept().await?; // Wait for new connection
-            let transmitter = self.transmitter.clone(); // Clones transmitter so that ownership doesn't conflict with multiple users
-            let mut reciever = transmitter.subscribe(); // Don't forget to smash that like button and hit the bell so you never miss another video!
+            let clients = Arc::clone(&self.clients);
+            let rooms = Arc::clone(&self.rooms);
+            let global_tx = self.global_tx.clone(); // Clones transmitter so that ownership doesn't conflict with multiple users
+            let mut global_rx = global_tx.subscribe(); // Don't forget to smash that like button and hit the bell so you never miss another video!
 
             tokio::spawn(async move {
                 let (reader, mut writer) = socket.split(); // Splits socket into read/write halves
                 let mut reader = BufReader::new(reader);
+                let (private_tx, mut private_rx) = mpsc::unbounded_channel::<String>();
+                let mut current_user_id = String::new();
                 let mut line = String::new();
                 loop {
                     tokio::select! { // Allows for running the two processes concurrently
-                        read = reader.read_line(&mut line) => { // Proc 1: Read from bound client
-                            if read.unwrap_or(0) == 0 {
-                                break;
-                            } // Exit if client gets disconnected
-                            if serde_json::from_str::<Packet>(&line).is_ok() {
-                                let _ = transmitter.send(line.clone()); // Send JSON to everyone after checking for validity
+                        read = reader.read_line(&mut line) => {
+                            if read.unwrap_or(0) == 0 { break; }
+                            if let Ok(packet) = serde_json::from_str::<Packet>(&line) {
+                                // Update or set UID
+                                if current_user_id.is_empty() {
+                                    current_user_id = packet.source.clone();
+                                    clients.write().unwrap().insert(current_user_id.clone(), private_tx.clone());
+                                }
+                                match &packet.navi {
+                                    Navigation::Everyone => {
+                                        // Send to the broadcast channel
+                                        let _ = global_tx.send(line.clone());
+                                    },
+                                    Navigation::User(target) => {
+                                        // Direct Routing
+                                        let register = clients.read().unwrap();
+                                        if let Some(target_tx) = register.get(target) {
+                                            // Send only to the specific user/room pipe
+                                            let _ = target_tx.send(line.clone());
+                                        } else {
+                                            // [Optional: Send error back to user - Target Not Found]
+                                        }
+                                    },
+                                    Navigation::Room(room) => {
+                                        // Join room
+                                        {
+                                            let mut rooms_guard = rooms.write().unwrap();
+                                            rooms_guard.entry(room.clone()).or_insert_with(HashSet::new).insert(current_user_id.clone());
+                                        }
+                                        // Send to room
+                                        let rooms_guard = rooms.read().unwrap();
+                                        if let Some(members) = rooms_guard.get(room) {
+                                            let clients_guard = clients.read().unwrap();
+                                            for id in members {
+                                                if id != &current_user_id {
+                                                    if let Some(tx) = clients_guard.get(id) {
+                                                        let _ = tx.send(line.clone());
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
                             }
                             line.clear();
                         }
-                        read = reciever.recv() => { // Proc 2: Listen for msgs from other clients
-                            if let Ok(msg) = read {
-                                if writer.write_all(msg.as_bytes()).await.is_err() {
-                                    break;
-                                }
-                            }
+                        // Targeted recieved
+                        Some(msg) = private_rx.recv() => {
+                            if writer.write_all(msg.as_bytes()).await.is_err() { break; }
+                        }
+
+                        // Global recieved
+                        Ok(msg) = global_rx.recv() => {
+                            if writer.write_all(msg.as_bytes()).await.is_err() { break; }
                         }
                     }
                 }
+                // Cleanup
+                if !current_user_id.is_empty() {
+                    clients.write().unwrap().remove(&current_user_id);
+                    // Remove from rooms
+                    let mut rooms_guard = rooms.write().unwrap();
+                    for members in rooms_guard.values_mut() {
+                        members.remove(&current_user_id);
+                    }
+                    // Remove keys from set
+                    rooms_guard.retain(|_, members| !members.is_empty());
+                }
+                
             });
 
             // [Good place to log client disconnecting]
